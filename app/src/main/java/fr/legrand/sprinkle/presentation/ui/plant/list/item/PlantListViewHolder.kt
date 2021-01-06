6277715d3ba1d41f69030b55fd9426e7c17bcef6
blegrand <benjamin.legrand93@gmail.com>
2021-01-06 23:27:43 +0100

    Improved sprinkle date format

--- app/src/main/java/fr/legrand/sprinkle/presentation/ui/plant/list/item/PlantListViewHolder.kt
+++ app/src/main/java/fr/legrand/sprinkle/presentation/ui/plant/list/item/PlantListViewHolder.kt
@@ -9,7 +9,6 @@ import kotlinx.android.synthetic.main.view_plant_list_item.view.view_plant_list_
 import kotlinx.android.synthetic.main.view_plant_list_item.view.view_plant_list_item_last_sprinkle_date
 import kotlinx.android.synthetic.main.view_plant_list_item.view.view_plant_list_item_name
 import kotlinx.android.synthetic.main.view_plant_list_item.view.view_plant_list_item_next_sprinkle_date
-import kotlinx.android.synthetic.main.view_plant_list_item.view.view_plant_list_item_species
 import kotlin.time.ExperimentalTime
 
 @ExperimentalTime
@@ -18,7 +17,6 @@ class PlantListViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     fun bind(plant: PlantViewDataWrapper) {
         with(itemView) {
             view_plant_list_item_name.text = plant.getName()
-            view_plant_list_item_species.text = plant.getSpecies()
             view_plant_list_item_last_sprinkle_date.text =
                 plant.getFormattedLastSprinkleDate(context)
             view_plant_list_item_next_sprinkle_date.text =
--- app/src/main/java/fr/legrand/sprinkle/presentation/ui/wrapper/PlantViewDataWrapper.kt
+++ app/src/main/java/fr/legrand/sprinkle/presentation/ui/wrapper/PlantViewDataWrapper.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import fr.legrand.sprinkle.R
 import fr.legrand.sprinkle.data.model.Plant
 import java.text.SimpleDateFormat
-import java.util.Locale
+import java.util.*
+import kotlin.time.DurationUnit
 import kotlin.time.ExperimentalTime
+import kotlin.time.toDuration
 
 private const val SPRINKLE_DATE_FORMAT = "dd/MM/yy"
 
@@ -29,11 +31,24 @@ data class PlantViewDataWrapper(private val plant: Plant) {
 
     fun getFormattedNextSprinkleDate(context: Context): String =
         plant.nextSprinkleDate?.let { nextSprinkleDate ->
-            val dateFormat = SimpleDateFormat(SPRINKLE_DATE_FORMAT, Locale.getDefault())
-            return context.getString(
-                R.string.next_sprinkle_date_format,
-                dateFormat.format(nextSprinkleDate)
-            )
+            val remainingDays =
+                (Date().time - nextSprinkleDate.time).toDuration(DurationUnit.MILLISECONDS).inDays.toInt()
+            return when {
+                remainingDays < 0 -> {
+                    context.getString(R.string.next_sprinkle_date_reached)
+                }
+                remainingDays == 0 -> {
+                    context.getString(R.string.next_sprinkle_date_today)
+                }
+                else -> {
+                    context.resources.getQuantityString(
+                        R.plurals.next_sprinkle_date_format,
+                        remainingDays,
+                        remainingDays
+                    )
+                }
+            }
+
         } ?: context.getString(R.string.next_sprinkle_date_unknown)
 
     fun getImageUrl() = plant.imageUrl
