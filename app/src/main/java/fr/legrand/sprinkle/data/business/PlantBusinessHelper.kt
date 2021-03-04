2846511c3e7ebd5eb6d1ed20aced61f1aca72970
blegrand <benjamin.legrand93@gmail.com>
2021-03-04 21:54:44 +0100

    Fixed ktlint issues

--- app/src/main/java/fr/legrand/sprinkle/data/business/PlantBusinessHelper.kt
+++ app/src/main/java/fr/legrand/sprinkle/data/business/PlantBusinessHelper.kt
@@ -5,12 +5,12 @@ import javax.inject.Inject
 import javax.inject.Singleton
 import kotlin.time.ExperimentalTime
 
-//TODO put reusable when storage layer is done
+// TODO put reusable when storage layer is done
 @ExperimentalTime
 @Singleton
 class PlantBusinessHelper @Inject constructor() {
 
-    //TODO add storage layer
+    // TODO add storage layer
     private val plantList = mutableListOf<Plant>()
 
     fun retrievePlantList(): List<Plant> {
--- app/src/main/java/fr/legrand/sprinkle/presentation/di/module/PlantCreateActivityModule.kt
+++ app/src/main/java/fr/legrand/sprinkle/presentation/di/module/PlantCreateActivityModule.kt
@@ -16,5 +16,4 @@ object PlantCreateActivityModule {
     @ActivityScoped
     fun plantCreateFragmentNavigatorListener(navigator: PlantCreateNavigator): PlantCreateFragmentNavigatorListener =
         navigator
-
 }
